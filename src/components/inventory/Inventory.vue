32a06597fd3a3a9cc7193bd69e61ea60c916e8ef
mspbb <bio29s@mail.ru>
2023-09-26 17:17:26 +0700

    мне не нравится как все это получается

--- src/components/inventory/Inventory.vue
+++ src/components/inventory/Inventory.vue
@@ -131,25 +131,45 @@
         :key="block.name"
         :ref="setItemRef"
         draggable="true"
-        v-on:dragstart="drag"
-        v-on:click="click"
+        v-on:dragstart="drag(block)"
+        v-on:click="select(block)"
       >
         {{ block.name }}
       </div>
     </div>
 
-    <div class="modal" v-if="showModal">some text</div>
+    <div class="modal" v-if="showModal">
+      <div>{{ selectedBlock.name }}</div>
+      <div>path - {{ selectedBlock.path }}</div>
+      <div>color - {{ selectedBlock.color }}</div>
+    </div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, onUpdated, ref } from "vue";
-const squareExample = ref(null);
-
 let targetForDrag = ref(null);
-let showModal = ref(false);
+
 let slotsArr = ref([]);
 let itemRefs = ref([]);
+
+let showModal = ref(false);
+let selectedBlock = ref("");
+
+let itemsArr = ref([]);
+
+const localSave = JSON.parse(localStorage.getItem("itemSetting"));
+if (localSave) {
+  console.log(itemsArr.value);
+  console.log(localSave._value);
+  itemsArr.value = localSave._value;
+} else {
+  itemsArr.value = [
+    { name: "first", path: 0, ref: 0, color: "green" },
+    { name: "second", path: 1, ref: 1, color: "green" },
+    { name: "third", path: 7, ref: 2, color: "green" },
+  ];
+}
 
 function setItemRef(el) {
   if (el) {
@@ -157,47 +177,62 @@ function setItemRef(el) {
   }
 }
 
-let itemsArr = ref([
-  { name: "first", path: 0, ref: 0 },
-  { name: "second", path: 1, ref: 1 },
-  { name: "third", path: 7, ref: 2 },
-]);
-
 onMounted(() => {
   document.querySelectorAll(".inventory__slot").forEach((e) => {
     slotsArr.value.push(e);
   });
 
+  let refCount = 0;
   itemsArr.value.forEach((e) => {
-    slotsArr.value[e.path].append(itemRefs.value[e.ref]);
-    e.ref = itemRefs.value[e.ref];
+    slotsArr.value[e.path].append(itemRefs.value[refCount]);
+    e.ref = itemRefs.value[refCount];
+    refCount++;
   });
 });
 
-function click(e) {
+function select(e) {
+  selectedBlock = e;
+  console.log(selectedBlock.name);
+  openModal();
+}
+
+function openModal() {
+  showModal.value = false;
   if (!showModal.value) {
     showModal.value = true;
-    console.log(e.target);
-  } else {
-    showModal.value = false;
   }
 }
 
 function drag(e) {
-  targetForDrag = e.target;
+  targetForDrag = e;
 }
 function dragDrop(e) {
   e.preventDefault();
 
   if (e.target.classList.contains("inventory__slot")) {
-    var data = targetForDrag;
+    let data = targetForDrag.ref;
     e.target.appendChild(data);
+
+    slotsArr.value.forEach((el) => {
+      if (el == e.target) {
+        targetForDrag.path = slotsArr.value.indexOf(el);
+      }
+    });
+    localStorage.setItem("itemSetting", JSON.stringify(itemsArr));
   } else {
     let needsBlocks = e.composedPath();
     needsBlocks.forEach((e) => {
       if (e.className == "inventory__slot") {
-        var data = targetForDrag;
+        let data = targetForDrag.ref;
+
         e.appendChild(data);
+
+        slotsArr.value.forEach((el) => {
+          if (el == e.target) {
+            targetForDrag.path = slotsArr.value.indexOf(el);
+          }
+        });
+        localStorage.setItem("itemSetting", JSON.stringify(itemsArr));
       }
     });
   }
