5690248e8ef2541eeca734413e770027091a9c5f
mspbb <bio29s@mail.ru>
2023-09-26 15:37:10 +0700

    item render

--- src/components/inventory/Inventory.vue
+++ src/components/inventory/Inventory.vue
@@ -1,8 +1,124 @@
 <template>
   <div class="inventory__wrapper">
-    <div class="inventory__slot" v-on:drop="dragDrop" v-on:dragover="allowDrop">
-      <div class="inventory__item" draggable="true" v-on:dragstart="drag"></div>
-    </div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
+    <div
+      class="inventory__slot"
+      v-on:drop="dragDrop"
+      v-on:dragover="allowDrop"
+    ></div>
     <div
       class="inventory__slot"
       v-on:drop="dragDrop"
@@ -11,139 +127,57 @@
     <div class="inventory__slot" v-on:drop="dragDrop" v-on:dragover="allowDrop">
       <div
         class="inventory__item"
+        v-for="block in itemsArr"
+        :key="block.name"
+        :ref="setItemRef"
         draggable="true"
         v-on:dragstart="drag"
         v-on:click="click"
-      ></div>
+      >
+        {{ block.name }}
+      </div>
     </div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
-    <div
-      class="inventory__slot"
-      v-on:drop="dragDrop"
-      v-on:dragover="allowDrop"
-    ></div>
 
     <div class="modal" v-if="showModal">some text</div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { onMounted, onUpdated, ref } from "vue";
+const squareExample = ref(null);
 
 let targetForDrag = ref(null);
 let showModal = ref(false);
+let slotsArr = ref([]);
+let itemRefs = ref([]);
+
+function setItemRef(el) {
+  if (el) {
+    itemRefs.value.push(el);
+  }
+}
+
+let itemsArr = ref([
+  { name: "first", path: 0, ref: 0 },
+  { name: "second", path: 1, ref: 1 },
+  { name: "third", path: 7, ref: 2 },
+]);
+
+onMounted(() => {
+  document.querySelectorAll(".inventory__slot").forEach((e) => {
+    slotsArr.value.push(e);
+  });
+
+  itemsArr.value.forEach((e) => {
+    slotsArr.value[e.path].append(itemRefs.value[e.ref]);
+    e.ref = itemRefs.value[e.ref];
+  });
+});
 
 function click(e) {
   if (!showModal.value) {
     showModal.value = true;
+    console.log(e.target);
   } else {
     showModal.value = false;
   }
